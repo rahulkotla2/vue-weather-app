c55a26e34729e4b518148dff2bb9845ae9440056
rahulkotla2 <rahulkotla2@gmail.com>
2023-03-21 11:15:35 +0530

    Added Async Js and output the data on to the screen

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="head">
+  <div id="head" :class="checkClass">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search...">
+        <input type="text" class="search-bar" placeholder="Search..." v-model="query" @keypress="fetchWeather">
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }},{{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}&deg;c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
       </div>
     </main>
   </div>
@@ -13,8 +25,69 @@ export default {
   data() {
     return {
       api_key: 'fd8715bbd26c5f06ec357529266147e3',
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      api: '',
+      query: '',
+      weather: {},
     }
-  }
+  },
+  computed: {
+    checkClass() {
+      if (typeof this.weather.main != 'undefined' && this.weather.main.temp > 16) {
+        return 'warm';
+      }
+      return 'cold';
+    }
+  },
+  methods: {
+    fetchWeather(e = null) {
+      if (!e || e.key == "Enter") {
+        if (e) {
+          this.api = `${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`;
+        }
+        fetch(this.api)
+          .then(res => res.json())
+          .then(this.setResults)
+          .catch(err => {
+            console.log(err)
+          })
+      }
+    },
+    onSuccess(position) {
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+      this.api = `${this.url_base}weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${this.api_key}`;
+      this.fetchWeather();
+    },
+    onError(error) {
+      alert(error);
+    },
+    setResults(results) {
+      this.weather = results;
+      console.log(this.weather);
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
+    }
+  },
+  created() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.onSuccess, this.onError);
+    }
+    else {
+      alert("Your browser not support geolocation api");
+    }
+  },
 }
 </script>
 
@@ -29,11 +102,15 @@ body {
   font-family: 'montserrat', sans-serif;
 }
 
-.head {
+#head {
   background-image: url('./assets/cold-bg.jpg');
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#head.warm {
+  background-image: url('./assets/warm-bg.jpg');
 }
 
 main {
@@ -68,4 +145,55 @@ main {
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0 16px 0;
 }
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+@media screen and (min-width:709px) {
+  #head{
+    background-image: url('./assets/Clody.jpg');
+  }
+  #head.warm {
+    background-image: url('./assets/Sunny.jpg');
+  }
+} 
 </style>
